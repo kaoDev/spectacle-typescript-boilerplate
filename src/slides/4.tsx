ecfc219fa5e302bec6aa5a2ea203a89c6962b150
Kalle Ott <kalle.ott@cap3.de>
2018-08-16 12:23:16 +0200

    initial setup with updates and prettier

--- src/slides/4.tsx
+++ src/slides/4.tsx
@@ -1,11 +1,11 @@
-import * as React from 'react'
-import { Slide, BlockQuote, Quote, Cite } from 'spectacle'
+import * as React from "react"
+import { Slide, BlockQuote, Quote, Cite } from "spectacle"
 
 export default (
-    <Slide transition={['fade']} bgColor="secondary" textColor="primary">
-        <BlockQuote>
-            <Quote>Example Quote</Quote>
-            <Cite>Author</Cite>
-        </BlockQuote>
-    </Slide>
+  <Slide transition={["fade"]} bgColor="secondary" textColor="primary">
+    <BlockQuote>
+      <Quote>Example Quote</Quote>
+      <Cite>Author</Cite>
+    </BlockQuote>
+  </Slide>
 )
